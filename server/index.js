76b58bb48d23e8e2cb1a71ec0ce218e16dde523d
Gen <zenoplex@gmail.com>
2016-06-14 08:36:41 +0900

    chore: add server for dev

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const webpack = require('webpack');
+const path = require('path');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('../webpack.config');
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+  noInfo:     true,
+  publicPath: config.output.publicPath,
+  stats:      { colors: true },
+}));
+
+app.use(webpackHotMiddleware(compiler));
+
+const server = app.listen(3000, '0.0.0.0', err => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const { port } = server.address();
+  console.log(`Listening at http://localhost:${port}`);
+});
